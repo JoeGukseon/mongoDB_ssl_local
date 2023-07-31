78e498bb5e5c3284fe44bac1eee7f65cd1ab1724
JoeGukSeon <zipcks1381@gmail.com>
2023-08-01 00:03:15 +0900

    Initial commit

---
+++ src/main/java/com/test/mongodb_ssl_local/MongoDbSslLocalTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.mongodb_ssl_local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbSslLocalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbSslLocalTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/mongodb_ssl_local/MongoDbSslLocalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.mongodb_ssl_local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbSslLocalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
